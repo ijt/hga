6867b8560883435efed3de63e68172e8111e640a
Issac Trotts <issac.trotts@gmail.com>
2011-03-30 09:00:45 -0700

    Add taylor expansions.

--- NumFun.hs
+++ NumFun.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 
+import Control.Applicative
+
 -- Floating-point valued functions that take a single float as input.
 type Flo2 = Float -> Float
 
 -- HACK.
 -- Show all Flo2s the same way to satisfy the Show requirement on the Num
 -- typeclass.
---instance Show Flo2 where
-    --show f = "<function R -> R>"
+instance Show Flo2 where
+    show f = "<function R -> R>"
 
 -- HACK.
 -- Provide a non-useful definition of equality for Flo2s so we can
@@ -33,4 +35,25 @@ instance Num Flo2 where
 instance Fractional Flo2 where
     f / g = \x -> f x / g x
     fromRational r = \x -> fromRational r
+
+data Taylor = Taylor [Flo2]
+
+instance Show Taylor where
+    show t = "<Taylor expansion>"
+
+-- Given a list of a function's derivatives starting with order 0, and a central
+-- point, give back terms of a Taylor expansion.
+taylor :: [Flo2] -> Float -> Taylor
+taylor fs x0 = 
+    Taylor [\x -> (x-x0)^k * dfk x0 / factorialf k | (k, dfk) <- zip [0..] fs]
+
+evalTaylor :: Taylor -> Int -> Float -> Float
+evalTaylor (Taylor fs) k x =
+    sum $ take k $ fs <*> [x]
+
+factorialf :: Integer -> Float
+factorialf k = fromIntegral $ factorial k
+ 
+factorial :: Integer -> Integer
+factorial k = product [1..k]
 
