209864ed9efb5e1960325319873295ea4d24dfd3
Issac Trotts <issac.trotts@gmail.com>
2011-03-31 22:31:51 -0700

    Add dot product, be better about normalizing blades.

--- BladeSum.hs
+++ BladeSum.hs
@@ -2,25 +2,41 @@ module BladeSum where
 
 import Data.List
 
+data NamedThing a = WithName { name :: String, contents :: a } deriving Show
+
 -- Multivector
-data Mv = BladeSum { mvTerms :: [Blade] } deriving Show
+data Mv = BladeSum { mvTerms :: [Blade] }
 
 instance Eq Mv where
     a == b = 
         mvTerms (mvNormalForm a) == mvTerms (mvNormalForm b)
 
+instance Show Mv where
+    show a =
+        stringJoin " + " $ map show $ mvTerms a
+
+stringJoin :: String -> [String] -> String
+stringJoin sep parts =
+    concat $ intersperse sep parts
+
 -- Scaled basis blade: the pseudoscalar for the space it spans.
--- These are stupid. If you ask for equality, no normalization is done.
--- TODO(ijt): Make a BladeNormalForm type that guarantees goodness.
-data Blade = Blade {bScale :: Float, bIndices :: [Int]} deriving (Show, Ord, Eq)
+-- These should always be returned in normal form so everyone can
+-- assume that any blade passed in is already in normal form.
+data Blade = Blade {bScale :: Float, bIndices :: [Int]} deriving (Ord, Eq)
+
+instance Show Blade where
+    show b =
+        (show $ bScale b) ++ "e" ++ (show $ bIndices b)
 
 -- Constructs a multivector from a scaled blade.
 e :: Float -> [Int] -> Mv
-s `e` indices = BladeSum $ [Blade s indices]
+s `e` indices = mvNormalForm $ BladeSum [Blade s indices]
 
 instance Num Mv where
     a + b = mvNormalForm $ BladeSum (mvTerms a ++ mvTerms b)
     a - b = mvNormalForm $ BladeSum (mvTerms a ++ (map bladeNeg $ mvTerms b))
+
+    -- 
     a * b = mvNormalForm $ BladeSum [bladeMul x y | x <- mvTerms a, y <- mvTerms b]
 
     fromInteger i = (fromIntegral i) `e` []
@@ -29,7 +45,7 @@ bladeNeg :: Blade -> Blade
 bladeNeg b = Blade (- bScale b) (bIndices b)
 
 bladeMul x y =
-    Blade (bScale x * bScale y) (bIndices x ++ bIndices y)
+    bladeNormalForm $ Blade (bScale x * bScale y) (bIndices x ++ bIndices y)
 
 bladeNonZero :: Blade -> Bool
 bladeNonZero b = bScale b /= 0
@@ -83,13 +99,32 @@ findFixedPoint f x =
   if y == x then y else findFixedPoint f y
   where y = f x
 
+-- The grade extraction operator
 getGrade :: Int -> Mv -> Mv
 getGrade k mv =
     BladeSum $ filter (`bIsOfGrade` k) (mvTerms mv)
 
+dot :: Mv -> Mv -> Mv
+dot a b =
+   BladeSum [x `bDot` y | x <- mvTerms a, y <- mvTerms b]
+
+grade :: Blade -> Int
+grade b = length $ bIndices b
+
+bGetGrade :: Int -> Blade -> Blade
+bGetGrade k b =
+    if b `bIsOfGrade` k then b else Blade 0 []
+
+bDot :: Blade -> Blade -> Blade
+bDot x y =
+    bladeNormalForm $ bGetGrade k xy
+        where
+            k = (abs (grade x) - (grade y))
+            xy = bladeMul x y
+
 bIsOfGrade :: Blade -> Int -> Bool
 blade `bIsOfGrade` k =
-    (length $ bIndices $ bladeNormalForm blade) == k
+    (length $ bIndices blade) == k
 
 -- TESTS
 
@@ -97,15 +132,16 @@ blade `bIsOfGrade` k =
 assertEqual :: (Eq a, Show a) => a -> a -> String -> IO ()
 assertEqual expected actual msg =
     if actual /= expected
-        then error $ msg ++ ": " ++ show actual ++ " /= " ++ show expected
+        then error $ msg ++ ": " ++ show expected ++ " /= " ++ show actual
         else putStrLn (msg ++ " passed.")
 
 main = do
-    assertEqual 1 (1`e`[]) "Scalar construction"
+    assertEqual 1 (1`e`[]) "Scalar construction 1 == 1e1"
     assertEqual 6 (2`e`[1] * 3`e`[1]) "Product of colinear vectors"
     assertEqual (-6`e`[1,2]) (2`e`[2] * 3`e`[1]) "Product of orthogonal vectors"
     assertEqual (6 - 6`e`[1,2]) ((2`e`[1] + 2`e`[2]) * 3`e`[1]) "Complex-like result"
-    --assertEqual 12 ((4`e`[1] + 2`e`[2]) `dot` 3`e`[1]) "Dot product"
+    assertEqual 1 ((1`e`[1]) `dot` (1`e`[1])) "Dot product e1 . e1"
+    assertEqual 12 ((4`e`[1] + 2`e`[2]) `dot` (3`e`[1])) "Dot product"
     assertEqual 3 (getGrade 0 3) "Zero grade part of a scalar"
     assertEqual 0 (getGrade 0 (1 `e` [1])) "Zero grade part of a vector"
     assertEqual 0 (getGrade 1 3) "One-grade part of a scalar"
